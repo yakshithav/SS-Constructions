20c210458f241fb85550b3d125bab853ca40b2f5
yakshithav <52580084+yakshithav@users.noreply.github.com>
2025-10-15 20:58:50 +0530

    Latest changes

---
+++ ss/webapp/controller/Home.controller.js
@@ -0,0 +1,81 @@
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast"],
+  function (Controller, MessageToast) {
+    "use strict";
+
+    return Controller.extend("ss.controller.Home", {
+      onInit: function () {
+        // Initialize any popover references if needed
+      },
+
+      onNavHome: function () {
+        this.getOwnerComponent().getRouter().navTo("RouteHome");
+      },
+
+      onNavAbout: function () {
+        this.getOwnerComponent().getRouter().navTo("RouteAbout");
+      },
+
+      onNavServices: function () {
+        this.getOwnerComponent().getRouter().navTo("RouteServices");
+      },
+
+      onNavPortfolio: function () {
+        this.getOwnerComponent().getRouter().navTo("RoutePortfolio");
+      },
+
+      onNavProjects: function () {
+        this.getOwnerComponent().getRouter().navTo("RouteProjects");
+      },
+
+      onNavContact: function () {
+        this.getOwnerComponent().getRouter().navTo("RouteContactUs");
+      },
+
+      onCallPress: function () {
+        window.open("tel:+918867506736", "_self");
+      },
+
+      onWhatsAppPress: function () {
+        window.open("https://wa.me/917019089330", "_blank");
+      },
+
+      onEmailPress: function () {
+        window.open("mailto:sidducivil07@gmail.com", "_self");
+      },
+
+      onMobileMenuPress: function (oEvent) {
+        var oButton = oEvent.getSource();
+        var oPopover = this.byId("mobileNavPopover");
+
+        if (oPopover) {
+          oPopover.openBy(oButton);
+        } else {
+          MessageToast.show("Navigation menu not available");
+        }
+      },
+
+      onFacebookPress: function () {
+        window.open("https://facebook.com/yourpage", "_blank");
+      },
+
+      onInstagramPress: function () {
+        window.open("https://instagram.com/yourprofile", "_blank");
+      },
+
+      onYouTubePress: function () {
+        window.open("https://youtube.com/yourchannel", "_blank");
+      },
+
+      onKnowMore: function () {
+        window.open("tel:+918867506736", "_self");
+        window.open("https://wa.me/917019089330", "_blank");
+        window.open("mailto:sidducivil07@gmail.com", "_self");
+      },
+
+      onViewProjects: function () {
+        this.getOwnerComponent().getRouter().navTo("RouteProjects");
+      },
+    });
+  }
+);
